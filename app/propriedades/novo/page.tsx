1427c52d2f0fb5990026a5efc2b0a4e79e97a313
marceloaraujodev <codetrsh@gmail.com>
2025-02-05 21:36:01 -0300

    redirect when no session

--- app/propriedades/novo/page.tsx
+++ app/propriedades/novo/page.tsx
@@ -1,8 +1,17 @@
 'use client';
-
+import { redirect } from 'next/navigation';
 import { PropertyForm } from '@/components/property/PropertyForm';
+import { useSession } from 'next-auth/react';
+
 
 export default function NewPropertyPage() {
+  const { data: session, status } = useSession();
+
+  if (!session) {
+    redirect('/'); // Redirect to home page if user is not logged in
+  }
+
+  console.log(session)
   return (
     <div className="pt-24 pb-12">
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
