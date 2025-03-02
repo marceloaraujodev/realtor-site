ff2f4562f22b2d9c5d5a42ca0823c559ae0f8080
codetrsh <134286416+codetrsh@users.noreply.github.com>
2025-03-02 14:55:40 -0300

    jimp

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,6 +1,7 @@
 declare module 'lucide-react';
 declare module '@radix-ui/react-slot';
 declare module '@radix-ui';
+declare module 'jimp';
 declare module 'class-variance-authority' {
   export function cva(variants: string, options: any): any;
   export type VariantProps<T> = { variant?: string; size?: string };
--- utils/compressImages.ts
+++ utils/compressImages.ts
@@ -1,8 +1,22 @@
-import Sharp from 'sharp';
+// import Sharp from 'sharp';
 
-export async function compressImage(fileBuffer: Buffer): Promise<Buffer>{
-  return await Sharp(fileBuffer)
-        .resize({ width: 1200 }) // Resize to a max width (optional) move to .env
-        .toFormat("webp", { quality: 80 }) // Convert to WebP with 80% quality
-        .toBuffer();
- }
+
+// export async function compressImage(fileBuffer: Buffer): Promise<Buffer>{
+//   return await Sharp(fileBuffer)
+//         .resize({ width: 1200 }) // Resize to a max width (optional) move to .env
+//         .toFormat("webp", { quality: 80 }) // Convert to WebP with 80% quality
+//         .toBuffer();
+//  }
+
+import * as Jimp from 'jimp';
+
+
+export async function compressImage(fileBuffer: Buffer) {
+  const image = await Jimp.read(fileBuffer); // Read the image from the buffer
+  await image
+    .resize(1200, Jimp.AUTO) // Resize to 1200px wide, keeping aspect ratio
+    .quality(80); // Set JPEG quality to 80%
+
+  return image.getBufferAsync(Jimp.MIME_JPEG); // Return the compressed image as a Buffer
+}
+
