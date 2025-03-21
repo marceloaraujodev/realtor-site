b47fce7ec231f66fda24d368206b73975698ff34
marceloaraujodev <codetrsh@gmail.com>
2025-03-21 00:28:54 -0300

    framer motion

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,9 @@
 'use client'
+import { motion } from 'framer-motion';
+
 export default function Hero() {
   return (
-    <div className="relative h-[600px] border">
+    <div className="relative h-[600px]">
       <div 
         className="absolute inset-0 bg-cover bg-center"
         style={{
@@ -11,17 +13,27 @@ export default function Hero() {
         <div className="absolute inset-0 bg-black/50" />
       </div>
       
-      <div className="relative -mt-4 sm:mt-0 h-full max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col justify-center items-center text-center">
+      <motion.div
+        initial={{y: '-450px', opacity: 0}}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1, ease: "easeOut" }}
+        className="relative -mt-4 sm:mt-0 h-full max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col justify-center items-center text-center">
         <h1 className="text-2xl sm:text-4xl md:text-6xl font-bold text-white mb-3">
           Encontre o imóvel dos seus sonhos em Balneário Camboriú
         </h1>
         <p className="max-w-xs sm:text-xl md:text-2xl text-white mb-10">
           Casas, apartamentos e terrenos disponíveis para venda ou locação
         </p>
-      </div>
-        <div className="h-[250px] -right-5 sm:h-[550px] absolute bottom-0 sm:right-0 ">
-          <img className="h-full" src='/profile-cutout.png' />
-        </div>
+      </motion.div>
+
+        <motion.div
+        initial={{ y: "100%", opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1, ease: "easeOut" }}
+        className="absolute h-[250px]  sm:h-[550px] bottom-0 sm:right-0"
+      >
+        <img className="h-full" src="/profile-cutout.png" />
+      </motion.div>
     </div>
   );
 }
